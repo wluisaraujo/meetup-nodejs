b030d1dcc813c04ef57b33d0bda29be7cdfd60ce
wluisaraujo <w.luis.araujo@gmail.com>
2021-02-18 21:14:56 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 const http = require('http')
 const PORT = 3000
 
+const DEFAULT_HEADER = { 'Content-Type': 'application/json' }
+
+const routes = {
+    'hookahs:get': async (request, response) => {
+
+    },
+    default: async (request, response) => {
+        response.end('404')
+
+    },
+}
 
 const handler = (request, response) => {
-    response.end('Hello')
+    const { url, method } = request
+    const [first, route, param] = url.split('/')
+    request.queryString = { param }
+
+    const key = `${route}:${method.toLowerCase()}`
+
+    response.writeHead(200, DEFAULT_HEADER)
+
+    const chosen = routes[key] || routes.default
+
+    return chosen(request, response)
 }
 
 http.createServer(handler)
